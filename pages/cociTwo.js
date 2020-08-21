359d9385afe14b75665324886f2e06eb2b02d4cc
dave-torr <djjabbua@gmail.com>
2020-08-20 21:36:28 -0500

    back in business modafaka

--- pages/cociTwo.js
+++ pages/cociTwo.js
@@ -6,42 +6,50 @@ import Pixel from '../components/Pixel'
 
 let fontfaceblk =  "./assets/logoAndVar/fontface-blk.png";
 
-let hortalizas = ["Lechuga roja", "Lechuga repollo", "Rúcola", "Acelga", "Apio", "Perejil Crespo", "Espinaca", "Radicho" ]
+import varieties from "../data/options.json";
 
 export default function CociliberVerTwo(props){
+
+    console.log("varieties", varieties)
     
     const [pickedVeggie, setPickedVeggie] = useState("rucola")
 
-    const recipieSec =(secTitle, plantType)=>{
-
-
-
+    const recipieSec =(variety)=>{
         return(
             <div className={styles.plantCatContainer} >
-                <div className={styles.plantCatTitle} > {secTitle} </div>
-                <div className={styles.varietySelector} > 
-                    {plantType}
-                </div>
+                <div className={styles.plantCatTitle} > {variety.description} </div>
+
             </div>
         )
     }
 
+
+
+
+
+
+
+
+
 return(
     <>
     <GeneralLayout>
-    <div style={{minHeight: "80vh"}} > 
-    <div className={styles.CociLiberIntroCont} >
-        <div className={styles.cLIntroTitle} > Coci <p style={{fontSize:"0.5em"}} > &#9752; </p> Liber </div>
-        <div className={styles.cLIntroSubtitle} > Deliciosos sabores <br></br> preparados con tus plantas <br></br> criadas en casa. </div>
-    </div>
+        <div style={{minHeight: "80vh"}} > 
+            <div className={styles.CociLiberIntroCont} >
+                <div className={styles.cLIntroTitle} > Coci <p style={{fontSize:"0.5em"}} > &#9752; </p> Liber </div>
+                <div className={styles.cLIntroSubtitle} > Deliciosos sabores <br></br> preparados con tus plantas <br></br> criadas en casa. <br></br> </div>
+            </div>
+            <div className={styles.RecipieGenCont} > 
+                <img src={fontfaceblk} className={styles.iconBlk} />
+                
 
-    <div className={styles.RecipieGenCont} > 
-        <img src={fontfaceblk} className={styles.iconBlk} />
-        
-        {recipieSec("Hortalizas de Hoja", hortalizas )}
-    </div>
 
-    </div>
+                {recipieSec(varieties[0] )}
+            
+            
+            
+            </div>
+        </div>
     </GeneralLayout>
     </>
 )}
