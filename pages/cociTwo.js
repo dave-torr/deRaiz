afa1c2e121792bad153c3628c0343a92da14d76c
dave-torr <djjabbua@gmail.com>
2020-08-20 22:27:04 -0500

    advances in Coci Liber Collapsible and much more

--- pages/cociTwo.js
+++ pages/cociTwo.js
@@ -13,20 +13,43 @@ export default function CociliberVerTwo(props){
     console.log("varieties", varieties)
     
     const [pickedVeggie, setPickedVeggie] = useState("rucola")
+    const [visDiv, setVisDiv] = useState(styles.EachPlantSelector)
+    const [visDiv2, setVisDiv2] = useState(styles.HiddenEachPlant)
 
-    const recipieSec =(variety)=>{
+
+
+    const recipieSec =(variety, visController)=>{
+        let eachPlant = variety.options.map((eachItem)=>
+            <p className={styles.eachPlant} >  {eachItem}  </p> )
+
         return(
             <div className={styles.plantCatContainer} >
                 <div className={styles.plantCatTitle} > {variety.description} </div>
-
+                <p id={variety.plantType} onClick={()=> collapseController(variety) } > collapse </p> 
+                <div className={visController}>
+                 {eachPlant}
+                </div> 
             </div>
         )
     }
 
-
-
-
-
+    const collapseController =(eachVariaty)=>{
+        if (eachVariaty.plantType === "hortalizas" ){
+            console.log( "Horta BIATCH")
+            if(visDiv===styles.EachPlantSelector){
+                setVisDiv(styles.HiddenEachPlant)
+            } else if (visDiv===styles.HiddenEachPlant){
+                setVisDiv(styles.EachPlantSelector)
+            }
+        } else if (eachVariaty.plantType === "aromáticas"){
+            console.log( "Aroma of Christ Church")
+            if(visDiv2===styles.EachPlantSelector){
+                setVisDiv2(styles.HiddenEachPlant)
+            } else if (visDiv2===styles.HiddenEachPlant){
+                setVisDiv2(styles.EachPlantSelector)
+            }
+        }
+    }
 
 
 
@@ -44,7 +67,8 @@ return(
                 
 
 
-                {recipieSec(varieties[0] )}
+                {recipieSec(varieties[0], visDiv ) }
+                {recipieSec(varieties[1], visDiv2 ) }
             
             
             
