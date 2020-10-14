7369096931597c45ed1848e685bc92916f4c26b1
dave-torr <djjabbua@gmail.com>
2020-10-13 19:01:01 -0500

    catalogue correction and photo addition. now finishing up user creation, with profile pic and data

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import Pixel from '../components/Pixel'
 
 //Product Info
 let tradOne={
-    "type": "Tradicional en Tierra",
+    "type": "De Meson en Tierra",
     "name": "Mini-Huerto Piramidal",
     "price": 18,
     "details": [
@@ -15,12 +15,12 @@ let tradOne={
         "Ideal para mesón",
         "Disponible en varios colores"
         ],
-    "measurements": "40cm x 20cm x 20cm",    
+    "measurements": "24cm x 24cm x 20cm",    
 }
-let tradOneImg = "./assets/productImgs/piramidSmall.png"
+let tradOneImg = "./assets/productImgs/pramidSmallOne.jpg"
 
 let tradTwo={
-    "type": "Tradicional en Tierra",
+    "type": "De Meson en Tierra",
     "name": "Mini-Huerto Piramidal Grande",
     "price": 28,
     "details": [
@@ -29,22 +29,35 @@ let tradTwo={
         "Ideal para mesón",
         "Disponible en varios colores"
         ],
-    "measurements": "40cm x 20cm x 20cm",    
+    "measurements": "36cm x 36cm x 30cm",    
 }
-let tradTwoImg = "./assets/productImgs/piramidLarge.png"
+let tradTwoImg = "./assets/productImgs/pyramidLrg.jpg"
 
-let tradThree={
-    "type": "Tradicional en tierra",
+let hangOne={
+    "type": "Colgante en tierra",
     "name": "Mini-Huerto Colgante",
     "price": 18,
     "details": [
-        "3 plantitas (pueden ser especies variadas)",
-        "Sistema modular colgante",
+        "2 plantitas (pueden ser especies variadas)",
+        "Util para llaves y otros elementos de uso diario!",
         "Disponible en varios colores"
         ],
-    "measurements": "40cm x 20cm x 20cm",    
+    "measurements": "34cm x 12cm x 20cm",    
 }
-let tradThreeImg = "./assets/productImgs/hangingOne.png"
+let hangOneImg = "./assets/productImgs/hangingOne.jpg"
+
+let hangTwo={
+    "type": "Colgante en tierra",
+    "name": "Mini-Huerto Colgante",
+    "price": 28,
+    "details": [
+        "3 plantitas (pueden ser especies variadas)",
+        "Elegante diseño colgante",
+        "Disponible en varios colores"
+        ],
+    "measurements": "24cm x 75cm x 20cm",    
+}
+let hangTwoImg = "./assets/productImgs/hangingTwo.jpg"
 
 let tradFour={
     "type": "Tradicional en tierra",
@@ -74,6 +87,8 @@ let hydroOne={
     "measurements": "40cm x 20cm x 20cm",    
 }
 let hydroOneImg = "./assets/productImgs/tubular1.png"
+let hydroOneImg2 = "./assets/productImgs/hydroTwo.jpg"
+let hydroOneImg23 = "./assets/productImgs/hydroTwoTwo.jpg"
 
 let hydroTwo={
     "type": "Hydrophonic",
@@ -93,7 +108,7 @@ let hydroTwoImg = "./assets/productImgs/squaredOne.png"
 let kiddiOne={
     "type": "Huerto Infantil",
     "name": "Mi Primer Huerto",
-    "price": 15,
+    "price": 18,
     "details": [
         "2 plantitas", 
         "Pinturas y Pincel", 
@@ -102,10 +117,13 @@ let kiddiOne={
         ],
     "measurements": "20cm x 25cm",    
 }
+let kiddiOneImg = "./assets/productImgs/kiddiOne.png"
+
+
 let kiddiTwo={
     "type": "Huerto Infantil",
     "name": "Mi Primer Huerto",
-    "price": 18,
+    "price": 20,
     "details": [
         "5 plantitas", 
         "Pinturas y Pincel", 
@@ -114,11 +132,9 @@ let kiddiTwo={
         ],
     "measurements": "45cm x 25cm",    
 }
-let kiddiOneImg = "./assets/productImgs/kiddiOne.png"
 
 
 let prlxImg1="./assets/fillerImgs/farmVeggieCloseupOne.jpg"
-
 let closingImg= "./assets/fillerImgs/horizontalPlantCover.png"
 
 export default function Home(){
@@ -183,118 +199,162 @@ export default function Home(){
         </>)
     }
 
-    return(<>
+    const PDFLink=()=>{
+        return(<>
+        <p style={{textAlign: "center", fontSize: "2em", color: "cromson", fontWeight: "600" }}> Catálogo actualizado!! </p>
+            <br></br>
+            <br></br>
+        </>)
+    }
+
+    const homeIntro=()=>{
+        return(<>
+            <div className={styles.homeLogoGenContainer} >
+                <div>
+                    <img className={styles.homeFontFaceOne} alt="logo font face green" src="./assets/logoAndVar/fontface-lghtGrn.png" />
+                    <h1 className={styles.homeTitle}> HUERTOS</h1>
+                    <img className={styles.homeIconOne} alt="icon green" src="./assets/logoAndVar/icon-lightGrn.png" />
+                </div>
+            </div>
+
+            <div className={styles.homeTextOne} > 
+                <p style={{fontSize: "1.2em"}}> Tu alimentación más sana, fresca y cerca de ti </p>
+                <p style={{fontSize: "1.5em"}}> Finalmente huertos en casa </p>
+                <p style={{fontSize: "1.5em"}}> ¡diseñados para ti! </p>
+                <p style={{fontSize: "1.5em"}}> - </p>
+            </div>
+
+            <img className={styles.handOffHome} alt="Handing plants off" src="./assets/fillerImgs/handoffOne.png" />
+
+            <h2 style={{ marginLeft: "9%", marginTop: "45px", fontSize:"2.5em", fontWeight: "200" }}> 
+                Agricultura a pequeña escala: simplificada </h2>
+            <div>
+                <h3 style={{marginLeft: "12%", marginRight: "30px", marginTop: "15px", fontWeight: "400" }}> 
+                    D´Raíz te trae sistemas para crecer tus propios vegetales en casa, sin la necesidad de tener ningún conocimiento de agricultura. <br></br> Comidas más nutritivas, variadas y frescas todos los días.</h3>
+                <h3 style={{ marginLeft: "12%", marginRight: "30px", marginTop: "15px", fontWeight: "200"}}> 
+                    Elegantes y prácticos sistemas <i> hidropónicos </i>  o a <i>  base de tierra.</i>  Encuentra el lugar ideal y ¡listo! en poco tiempo podrás disfrutar de deliciosos sabores criados en casa. </h3>
+            </div>        
+        </>)
+    }
+    const homeScrollers=()=>{
+        return(<>
+            <div className={styles.homeProductAnchors} >
+                <h3 style={{textAlign:"center", fontSize: "1.5em" }}> 
+                Explora nuestras opciones de:
+                </h3>
+            </div>
+                <div className={styles.anchorContainer} style={{textAlign:"center", display: "flex", justifyContent:"center", alignItems: "center" }}>
+                    <div className={styles.anchor} onClick={()=>{
+                        let AnchorHidro = document.getElementById("hidroAnchor");
+                        AnchorHidro.scrollIntoView({behavior: "smooth"});
+                    }}> Huertos <br></br> Hidroponicos 
+                        <div className={styles.anchorDeco} > </div>
+                        </div>
+                    <div className={styles.anchor} onClick={()=>{
+                        let AnchorTerra = document.getElementById("hangAnchor");
+                        AnchorTerra.scrollIntoView({behavior: "smooth"});
+                    }}> Huertos Colgantes <br></br> En Tierra  
+                        <div className={styles.anchorDeco} > </div>
+                        </div>
+                    <div className={styles.anchor} onClick={()=>{
+                        let AnchorTerra = document.getElementById("terraTwoAnchor");
+                        AnchorTerra.scrollIntoView({behavior: "smooth"});
+                    }}> Huertos de Mesón <br></br> Y Piso En Tierra  
+                        <div className={styles.anchorDeco} > </div>
+                        </div>
+                    <div className={styles.anchor} onClick={()=>{
+                        let AnchorTerra = document.getElementById("kiddiAnchor");
+                        AnchorTerra.scrollIntoView({behavior: "smooth"});
+                    }}>Huertos <br></br> Infantiles 
+                        <div className={styles.anchorDeco} > </div>
+                        </div>
+                </div>        
+        </>)
+    }
+    const homeParrallax=()=>{
+        return(<>
+            <div className={styles.homeParallax} style={{
+                backgroundImage:`url(${prlxImg1})`,
+                width: "100%",
+                transform: `translate(0px, 0px)`,
+                minHeight: "180px",
+                backgroundSize: "100% auto",
+                backgroundAttachment: "fixed",
+                backgroundRepeat: "no-repeat",
+                backgroundPositionY: "center"
+                }}>
+                </div>         
+        </>)
+    }
+    const prodSliders=()=>{
+        return(
+            <>
+            <div className={styles.productBar} > 
+                <h2 className={styles.prodBarTitle} id="hidroAnchor" > Huertos Hidroponicos: </h2>
+                <div  style={{display: "flex", overflowX: "scroll",} }  >
+                {ProdDisplayer(hydroOne, hydroOneImg)};
+                {/* {ProdDisplayer(hydroTwo, hydroTwoImg)}; */}
+                </div>
+                {/* <p style={{textAlign: "center", fontSize: "1.3em"}} >
+                <i>&#8640; desplaza para más opciones: &#8640;</i> </p> */}
+            </div>
+            <div className={styles.productBar} > 
+                <h2 className={styles.prodBarTitle} id="hangAnchor" > Huertos Colgantes en Tierra: </h2>
+                <div  style={{display: "flex", overflowX: "scroll",} }  >
+                {ProdDisplayer(hangOne, hangOneImg)};
+                {ProdDisplayer(hangTwo, hangTwoImg)};
+                </div>
+                <p style={{textAlign: "center", fontSize: "1.3em"}} >
+                <i>&#8640; desplaza para más opciones: &#8640;</i> </p>
+            </div>
+            <div className={styles.productBar} > 
+                <h2 className={styles.prodBarTitle} id="terraAnchor" > Huertos en Tierra: </h2>
+                <div  style={{display: "flex", overflowX: "scroll",} }  >
+                {ProdDisplayer(tradOne, tradOneImg)};
+                {ProdDisplayer(tradTwo, tradTwoImg)};
+                </div>
+                <p style={{textAlign: "center", fontSize: "1.3em"}} >
+                <i>&#8640; desplaza para más opciones: &#8640;</i> </p>
+            </div>
+            <div className={styles.productBar} > 
+                <h2 className={styles.prodBarTitle} id="kiddiAnchor"> Huertos Infantiles: </h2>
+                <div   >
+                {ProdDisplayer(kiddiOne, kiddiOneImg)};
+                </div>
+                {/* <p style={{textAlign: "center", fontSize: "1.3em"}} >
+                <i>&#8640; desplaza para más opciones:: &#8640;</i> </p> */}
+            </div>
+            </>
+        )
+    }
+
+    const homeClosing=()=>{
+        return(
+            <>
+                <div className={styles.closingDiv} >
+                <img src={closingImg} style={{ width:"100%"}} />
+
+                <p style={{fontSize: "2em", borderBottom: "solid 2px crimson", paddingBottom: "5px"}} > Sé parte de un presente </p>
+                <p style={{fontSize: "3em", borderTop: "solid 1px lime", marginTop: "5px" }} > más </p>
+                <p style={{fontSize: "3em", borderBottom: "solid 1px lime", textTransform: "uppercase", marginBottom: "5px" }} > sustentable </p>
+
+                </div>
+            </>
+        )
+    }
+
+    return(
+    <>
     {homeHead()}
-
-<GeneralLayout>
-            <p style={{textAlign: "center", fontSize: "2em", color: "cromson", fontWeight: "600" }}> Catálogo actualizado!! </p>
-            <br></br>
-            <br></br>
-  <main className="homeMain">
-
-    <div className={styles.homeLogoGenContainer} >
-        <div>
-            <img className={styles.homeFontFaceOne} alt="logo font face green" src="./assets/logoAndVar/fontface-lghtGrn.png" />
-            <h1 className={styles.homeTitle}> HUERTOS</h1>
-            <img className={styles.homeIconOne} alt="icon green" src="./assets/logoAndVar/icon-lightGrn.png" />
-        </div>
-    </div>
-
-    <div className={styles.homeTextOne} > 
-        <p style={{fontSize: "1.2em"}}> Tu alimentación más sana, fresca y cerca de ti </p>
-        <p style={{fontSize: "1.5em"}}> Finalmente huertos en casa </p>
-        <p style={{fontSize: "1.5em"}}> ¡diseñados para ti! </p>
-        <p style={{fontSize: "1.5em"}}> - </p>
-    </div>
-
-    <img className={styles.handOffHome} alt="Handing plants off" src="./assets/fillerImgs/handoffOne.png" />
-
-    <h2 style={{ marginLeft: "9%", marginTop: "45px", fontSize:"2.5em", fontWeight: "200" }}> 
-        Agricultura a pequeña escala: simplificada </h2>
-    <div>
-        <h3 style={{marginLeft: "12%", marginRight: "30px", marginTop: "15px", fontWeight: "400" }}> 
-            D´Raíz te trae sistemas para crecer tus propios vegetales en casa, sin la necesidad de tener ningún conocimiento de agricultura. <br></br> Comidas más nutritivas, variadas y frescas todos los días.</h3>
-        <h3 style={{ marginLeft: "12%", marginRight: "30px", marginTop: "15px", fontWeight: "200"}}> 
-            Elegantes y prácticos sistemas <i> hidropónicos </i>  o a <i>  base de tierra.</i>  Encuentra el lugar ideal y ¡listo! en poco tiempo podrás disfrutar de deliciosos sabores criados en casa. </h3>
-    </div> 
-
-    <div className={styles.homeProductAnchors} >
-        <h3 style={{textAlign:"center", fontSize: "1.5em" }}> 
-        Explora nuestras opciones de:
-        </h3>
-    </div>
-        <div className={styles.anchorContainer} style={{textAlign:"center", display: "flex", justifyContent:"center", }}>
-            <div className={styles.anchor} onClick={()=>{
-                let AnchorHidro = document.getElementById("hidroAnchor");
-                AnchorHidro.scrollIntoView({behavior: "smooth"});
-            }}> Huertos <br></br> Hidroponicos 
-                <div className={styles.anchorDeco} > </div>
-                </div>
-            <div className={styles.anchor} onClick={()=>{
-                let AnchorTerra = document.getElementById("terraAnchor");
-                AnchorTerra.scrollIntoView({behavior: "smooth"});
-            }}> Huertos <br></br> En Tierra  
-                <div className={styles.anchorDeco} > </div>
-                </div>
-            <div className={styles.anchor} onClick={()=>{
-                let AnchorTerra = document.getElementById("kiddiAnchor");
-                AnchorTerra.scrollIntoView({behavior: "smooth"});
-            }}>Huertos <br></br> Infantiles 
-                <div className={styles.anchorDeco} > </div>
-                </div>
-        </div>
-
-    <div className={styles.homeParallax} style={{
-        backgroundImage:`url(${prlxImg1})`,
-        width: "100%",
-        transform: `translate(0px, 0px)`,
-        minHeight: "180px",
-        backgroundSize: "100% auto",
-        backgroundAttachment: "fixed",
-        backgroundRepeat: "no-repeat",
-        backgroundPositionY: "center"
-         }}>
-        </div>           
-
-    <div className={styles.productBar} > 
-        <h2 className={styles.prodBarTitle} id="hidroAnchor" > Huertos Hidroponicos: </h2>
-        <div  style={{display: "flex", overflowX: "scroll",} }  >
-        {ProdDisplayer(hydroOne, hydroOneImg)};
-        {ProdDisplayer(hydroTwo, hydroTwoImg)};
-        </div>
-        <p style={{textAlign: "center", fontSize: "1.3em"}} >
-        <i>&#8640; desplaza para más opciones: &#8640;</i> </p>
-    </div>
-    <div className={styles.productBar} > 
-        <h2 className={styles.prodBarTitle} id="terraAnchor" > Huertos en Tierra: </h2>
-        <div  style={{display: "flex", overflowX: "scroll",} }  >
-        {ProdDisplayer(tradOne, tradOneImg)};
-        {ProdDisplayer(tradTwo, tradTwoImg)};
-        {ProdDisplayer(tradThree, tradThreeImg)};
-        </div>
-        <p style={{textAlign: "center", fontSize: "1.3em"}} >
-        <i>&#8640; desplaza para más opciones: &#8640;</i> </p>
-    </div>
-    <div className={styles.productBar} > 
-        <h2 className={styles.prodBarTitle} id="kiddiAnchor"> Huertos Infantiles: </h2>
-        <div   >
-        {ProdDisplayer(kiddiOne, kiddiOneImg)};
-        </div>
-        {/* <p style={{textAlign: "center", fontSize: "1.3em"}} >
-        <i>&#8640; desplaza para más opciones:: &#8640;</i> </p> */}
-    </div>
-
-    <div className={styles.closingDiv} >
-      <img src={closingImg} style={{ width:"100%"}} />
-
-      <p style={{fontSize: "2em", borderBottom: "solid 2px crimson", paddingBottom: "5px"}} > Sé parte de un presente </p>
-      <p style={{fontSize: "3em", borderTop: "solid 1px lime", marginTop: "5px" }} > más </p>
-      <p style={{fontSize: "3em", borderBottom: "solid 1px lime", textTransform: "uppercase", marginBottom: "5px" }} > sustentable </p>
-
-     </div>
-  </main>
-</GeneralLayout>
-
-
+        <GeneralLayout>
+            {PDFLink()}
+            <main className="homeMain">
+                {homeIntro()}
+                {homeScrollers()}
+                {homeParrallax()}       
+                {prodSliders()}
+                {homeClosing()}
+            </main>
+        </GeneralLayout>
     </>)
 }
