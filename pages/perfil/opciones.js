f60c97fba18c98dc7b92448facea56cf88e435b7
dave-torr <djjabbua@gmail.com>
2020-10-20 14:36:24 -0500

    profile options laid out, need to add functionality abd feedback to each btn

--- pages/perfil/opciones.js
+++ pages/perfil/opciones.js
@@ -1,16 +1,20 @@
 import styles from "./../../styles/profileOptions.module.css"
 import { useUser } from "../../utils/userHook"
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 
 export default function ProfileOptions(){    
     ////////////////////////////////////////////////////////////////////////
     //HOOKS
     //HOOKS
-
+    const profilePictureRef = useRef();
+    const aliasRef = useRef();
+    const userTypeRef = useRef();
+    const ownedProductsRef = useRef();
     const [user, { mutate }] = useUser();
     const [addPic, setaddPic] =useState(false)
     const [erasePic, setErasePic] =useState(false)
+    const [isUpdated, setIsUpdated] =useState()
     useEffect(() => {
       if (!user) router.push('/registro')
     }, [user]);
@@ -21,81 +25,165 @@ export default function ProfileOptions(){
         return(<> </>)
     }
     const optionsIntro=()=>{
-
         return(<>
             <div className={styles.optionsIntroCont} > 
                 <div className={styles.optionsTitle}>
                 Opciones de usuario
                 </div>
                 <div className={styles.optionSubTitle} >
-                    <div className={styles.userNameIntro}> {user.name} </div>
+                    <div className={styles.userNameIntro}> {user&& <>{user.name}</>} </div>
                     <img 
                         src="../assets/logoAndVar/fontface-blk.png"
                         alt="D´Raiz Icon"
                         className={styles.fontLogoImg}                 
                     />
                 </div>
+                <div> {isUpdated} </div>
             </div>
         </>)
     }
 
-    console.log(user)
-    const generalOptionForm=()=>{
-            //with sliders for yes or no options, much like a phone uses!
+    const userDataOptions=()=>{
+        return(<>
+            <div className={styles.profPicOptions}>
+                <div className={styles.optionSectionTitle} >
+                    Opciones de Datos de Usuario
+                </div>
+                <div className={styles.formOptionContainer} >
+                    <div className={styles.formOptionLabel} >
+                        Actualiza tu Alias
+                    </div>
+                    {/* user type picker */}
+                </div>
+                <div className={styles.formOptionContainer} >
+                    <div className={styles.formOptionLabel} >
+                        Actualiza tu Nombre
+                    </div>
+                    {/* user type picker */}
+                </div>
+                <div className={styles.formOptionContainer} >
+                    <div className={styles.formOptionLabel} >
+                        Actualiza tu tipo de usuario
+                    </div>
+                    {/* user type picker */}
+                </div>
+                <div className={styles.formOptionContainer} >
+                    <div className={styles.formOptionLabel} >
+                        Actualiza tu clave a la red D´Raiz
+                    </div>
+                    {/* user type picker */}
+                </div>
+
+            </div>
+        </>)
+    }
+    const productOptions=()=>{
+        return(<>
+            <div className={styles.profPicOptions}>
+                <div className={styles.optionSectionTitle} >
+                    Opciones de Productos
+                </div>
+                <div className={styles.formOptionContainer} >
+                    <div className={styles.formOptionLabel} >
+                        Registra tus productos
+                    </div>
+                    {/* user type picker */}
+                </div>
+                <div className={styles.formOptionContainer} >
+                    <div className={styles.formOptionLabel} >
+                        Registra tus subscripciones
+                    </div>
+                    {/* user type picker */}
+                </div>
+            </div>
+        </>)
+    }
+
+    const ImageOptions=()=>{
         return(<> 
             <div>
-                <form>
-                    <div className={styles.profPicOptions} >
-                    <div className={styles.optionSectionTitle} >
-                        Opciones de Imagen de Perfil
-                    </div>
-                        {user.profilePic===null? 
-                        <> 
-                            <div className={styles.formOptionContainer} >
-                                <div className={styles.formOptionLabel} >
-                                    Sube una imagen de perfil!
-                                </div>
-                                {addPic?<> 
-                                    <div className={styles.formDeleteCancel} 
-                                    onClick={()=>setaddPic(false)} >
-                                        Cancelar
-                                    </div>
-                                    <div className={styles.formOptionAction} 
-                                    onClick={()=>setaddPic(false)} >
-                                        Añadir Imagen
-                                    </div>
-                                </>:<>
-                                    <div className={styles.formOptionAction} 
-                                    onClick={()=>setaddPic(true)}>
-                                        Elige una imagen!
-                                    </div>
-                                </>}
+                <form id="imgForm" onSubmit={(e)=>updateProfile(e)}>
+                <div className={styles.profPicOptions} >
+                <div className={styles.optionSectionTitle} >
+                    Opciones de Imagen de Perfil
+                </div>
+                    {user.profilePic===null? 
+                    <> 
+                        <div className={styles.formOptionContainer} >
+                            <div className={styles.formOptionLabel} >
+                                Sube una imagen de perfil!
                             </div>
-                        </>:<>
-                            <div className={styles.formOptionContainer} >
-                                <div className={styles.formOptionLabel} >
-                                    Elimina tu foto de Perfil
+                            {addPic?<> 
+                                <div className={styles.formDeleteCancel} 
+                                onClick={()=>setaddPic(false)} >
+                                    Cancelar
                                 </div>
-                                {erasePic ? 
-                                <>
-                                    <div className={styles.formDelete} 
-                                    onClick={()=>setErasePic(false)} >
-                                        Eliminar Imagen
+                                <div style={{width:"200px"}}>
+                                    <label htmlFor="avatar" className={styles.imageUploader}>
+                                    Elije tu imagen de Perfil!*
+                                        </label>
+                                    <input
+                                        type="file"
+                                        id="avatar"
+                                        name="avatar"
+                                        accept="image/png, image/jpeg"    
+                                        ref={profilePictureRef}
+                                        />
+                                    <br></br>
+                                    <div > *Formato .jpg o .png solamente 
                                     </div>
-                                    <div className={styles.formDeleteCancel} 
-                                    onClick={()=>setErasePic(false)} >
-                                        Cancelar
-                                    </div>
-                                    
-                                </> : <> 
-                                <div className={styles.formDelete} onClick={()=>setErasePic(true)} >
-                                    borrar imagen
+                                    <button className={styles.submitBTN} type="submit" value="imgForm"> Subir foto!</button>
                                 </div>
-                                </>}
+                            </>:<>
+                                <div className={styles.formOptionAction} 
+                                onClick={()=>setaddPic(true)}>
+                                    Elige una imagen!
+                                </div>
+                            </>}
+                        </div>
+                    </>:<>
+                        <div className={styles.formOptionContainer} >
+                            <div className={styles.formOptionLabel} >
+                                Elimina tu foto de Perfil
                             </div>
-                        </>}
-                    </div>
-                </form>
+                            {erasePic ? 
+                            <>
+                                <div className={styles.formDelete} 
+                                onClick={async()=>{setErasePic(false)
+                                let formData = new FormData();
+                                formData.append('profilePicture', null)
+                                formData.append('alias', user.alias)
+                                formData.append('userType', user.userType)
+                                formData.append('ownedProducts', user.ownedProducts)
+                                const res = await fetch('/api/profileUpdate', {
+                                        method: 'PATCH',
+                                        body: formData,
+                                    });
+                                if (res.status === 200) {
+                                    const userData = await res.json();
+                                    mutate({
+                                        user: {
+                                        ...user,
+                                        ...userData.user,
+                                        },
+                                    });
+                                setIsUpdated("Foto Eliminada")    
+                                }}}>
+                                    Eliminar Imagen
+                                </div>
+                                <div className={styles.formDeleteCancel} 
+                                onClick={()=>setErasePic(false)} >
+                                    Cancelar
+                                </div>
+                            </> : <> 
+                            <div className={styles.formDelete} onClick={()=>setErasePic(true)} >
+                                borrar imagen
+                            </div>
+                            </>}
+                        </div>
+                    </>}
+                </div>
+            </form>
             </div>
         </>)
     }
@@ -103,16 +191,45 @@ export default function ProfileOptions(){
 
         return(<> 
             <div className={styles.userOptsGenCont} >
-                {generalOptionForm()}
+                {userDataOptions()}
+                {productOptions()}
+                {ImageOptions()}
             </div>
         </>)
     }
     ////////////////////////////////////////////////////////////////////////
     //FUNCTIONALITY
     //FUNCTIONALITY
-    const eliminateProfilePic=(e)=>{
-
+    const updateProfile=async(e)=>{
+        e.preventDefault();
+        let formData = new FormData();
+        if (profilePictureRef.current.files[0]) { 
+            formData.append('profilePicture', profilePictureRef.current.files[0])} else {formData.append('profilePicture', user.profilePicture)}
+        if (aliasRef.current===undefined){
+            formData.append('alias', user.alias)
+            } else {formData.append('alias', aliasRef.current.value)}
+        if (userTypeRef.current===undefined){
+            formData.append('userType', user.userType)
+            } else {formData.append('userType', userTypeRef.current.value)}
+        if (ownedProductsRef.current===undefined){
+            formData.append('ownedProducts', user.ownedProducts)
+            } else {formData.append('ownedProducts', ownedProductsRef.current.value)}
+        const res = await fetch('/api/profileUpdate', {
+                method: 'PATCH',
+                body: formData,
+            });
+        if (res.status === 200) {
+            const userData = await res.json();
+            mutate({
+                user: {
+                ...user,
+                ...userData.user,
+                },
+            });
+        setIsUpdated("Actualizacion Exitosa!")        
+        }                    
     }
+
 
     return(
         <><div style={{display: "flex", justifyContent:"center"}} > 
